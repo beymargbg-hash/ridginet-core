94e9cc63dcad06a8b916d61c796efa242b3be9bd
beymargbg-hash <beymarg.bg@gmail.com>
2026-03-06 08:30:41 -0300

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Asegúrate de que index.html esté dentro de la carpeta 'templates'
+    # Buscamos el nuevo sistema v2.0
     return render_template('index.html')
 
-if __name__ == "__main__":
-    # Railway le da el puerto a la App automáticamente
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    # Railway asigna el puerto automáticamente
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
