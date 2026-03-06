812d5ac541f8a24142aed725fae956e9d72ed211
beymargbg-hash <beymarg.bg@gmail.com>
2026-03-06 08:06:24 -0300

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-import os
 from flask import Flask, render_template
+import os
 
-# Definimos la app
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Railway buscará index.html dentro de la carpeta 'templates'
+    # Asegúrate de que index.html esté dentro de la carpeta 'templates'
     return render_template('index.html')
 
-if __name__ == '__main__':
-    # Captura el puerto que Railway asigna (usamos 5000 por defecto)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    # Railway le da el puerto a la App automáticamente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
